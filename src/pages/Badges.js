78588561b6a0940e58238fc0cbe96166296a9cb1
Daniel Vasquez <danielvasquez@Mac-mini-de-Daniel.local>
2021-03-25 14:41:19 -0600

    Clase 19 División de la aplicación en rutas

--- src/pages/Badges.js
+++ src/pages/Badges.js
@@ -5,6 +5,8 @@ import confLogo from "../images/badge-header.svg";
 import data from "../data.js";
 import BadgesList from "../components/BadgesList.js";
 
+import { Link } from "react-router-dom";
+
 class Badges extends React.Component {
   state = {
     data,
@@ -29,9 +31,9 @@ class Badges extends React.Component {
 
         <div className="Badges__container">
           <div className="Badges__buttons">
-            <a href="/badges/new" className="btn btn-primary">
+            <Link to="/badges/new" className="btn btn-primary">
               New Badge
-            </a>
+            </Link>
           </div>
 
           <div className="Badges__list">
