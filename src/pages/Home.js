99faac5e458087b0c56205449705d7ffdca63a15
Daniel Vasquez <danielvasquez@Mac-mini-de-Daniel.local>
2021-04-26 16:26:51 -0500

    Clase 35 Hooks

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,11 +2,33 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "../components/styles/Home.css";
 
-function Home(badge) {
+function useIncreaseCount(max) {
+  const [count, setCount] = React.useState(0);
+
+  if (count > max) {
+    setCount(0);
+  }
+
+  return [count, setCount];
+}
+
+function Home() {
+  const [count, setCount] = useIncreaseCount(7);
+
+  // const count = 4;
+
   return (
     <React.Fragment>
       <div className="Home">
         <h1>Badges Arcade.</h1>
+        <button
+          onClick={() => {
+            setCount(count + 1);
+          }}
+          className="btn btn-primary mt-4 mb-4"
+        >
+          Increase Count: {count}
+        </button>
         <Link to="/badges" className="btn btn-primary">
           Badge
         </Link>
